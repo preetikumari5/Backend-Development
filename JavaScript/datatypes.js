1b32e308992f2480234f2c76217b138b4ada42b7
preetikumari5 <147963preetikumari@gmail.com>
2024-07-27 11:02:48 +0530

    JS

--- JavaScript/datatypes.js
+++ JavaScript/datatypes.js
@@ -5,6 +5,44 @@
 // string
 var name = "Preeti"
 var title = "Kumari"
-console.log(name)
-
 console.log(typeof(name))
+
+// number
+var age = 91
+var x = 5.3
+console.log(typeof(x))
+
+// bigger number
+var big_num = 123n
+console.log(typeof big_num)
+
+console.log(typeof NaN)
+
+
+// null   this is an exception(type is null but it show object) 
+console.log(typeof null)
+
+// undefined value is not assigned
+var a 
+console.log(typeof a)
+
+// boolean
+console.log(typeof true)
+
+// symbol
+var s1 = Symbol()
+console.log(typeof s1)
+
+
+/**
+ * non primitive
+ */
+
+// objects
+var person = {
+    name: "Preeti",
+    age: 20,
+    hobby:"Reading"
+}
+console.log(person)
+console.log(typeof person)
