b61cda3b697ce5d30810f6a4f75dee54e5c882ac
preetikumari5 <147963preetikumari@gmail.com>
2024-07-27 11:12:45 +0530

    JS

---
+++ JavaScript/operators.js
@@ -0,0 +1,36 @@
+/**
+ * Assignment operators
+ */
+
+
+//  "="
+var name = "Preeti"
+
+// arithmetic operators
+console.log(4+5)
+console.log(4*5)
+console.log(4**5)  
+console.log(123445678%98)
+
+
+// incrmenting
+var i =1
+i = i+1
+console.log(i)
+
+var j =3
+console.log(j++)
+console.log(j)
+
+var k =4
+console.log(++k)
+console.log(k)
+
+
+// relational operator
+// equality
+console.log(1==2)
+console.log(1=='1')
+
+// strict equality
+console.log(1==='1')
