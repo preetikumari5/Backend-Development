26d756643d5d0d1ab50a019c0744ceddde9b2fe0
preetikumari5 <147963preetikumari@gmail.com>
2024-08-01 10:31:39 +0530

    destructuring

---
+++ JavaScript/destructring.js
@@ -0,0 +1,20 @@
+// destructring in array
+const nums = [1,2,3,4,5]
+const [a,b,c,d,e,f] = [1,2,3,4,5]
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(f)
+
+
+// object
+
+const user = {
+    name: 'preeti',
+    age : 20,
+    city: 'chennai'
+}
+
+const {name,age} = user
+console.log(name)
+console.log(age)
