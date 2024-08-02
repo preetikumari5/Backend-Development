e21686077d45bb4f0be972e7b2b99533b8e197e7
preetikumari5 <147963preetikumari@gmail.com>
2024-08-02 10:04:55 +0530

    synchronous execution

---
+++ JavaScript/sync.js
@@ -0,0 +1,14 @@
+// synchronous execution - execution works in stepwise manner
+
+console.log("Hello from first line")
+
+function hello(){
+    console.log('Hello from inside the function')
+}
+
+hello()
+
+console.log('Hello from the last line')
+
+
+
